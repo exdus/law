29ed34f5f803980211401c2ac1dac74e9bedfef5
keighb <keighb@live.com>
2020-10-10 05:35:33 +0300

    flask_mail and ajax functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template,request,abort,jsonify,Blueprint
+from flask import Flask, render_template,request,abort,jsonify,Blueprint,make_response
+from flask_mail import Mail,Message
 from flask_sqlalchemy import SQLAlchemy
 
 from instance.config import app_config
 
 db=SQLAlchemy()
+mail = Mail()
 
 def create_app(config_name):
 	#from app.models import User, Supplier, Products, Vendor
@@ -12,6 +14,7 @@ def create_app(config_name):
 	app.config.from_pyfile('config.py')
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 	db.init_app(app)
+	mail.init_app(app)
 
 	@app.route("/")
 	def landing_page():
@@ -46,9 +49,36 @@ def create_app(config_name):
 	def contact():
 		return render_template("contact.html")
 
+	@app.route('/message', methods=['GET', 'POST'])
+	def message():
+	    """User can send e-mail via contact form"""
+
+	    if request.method == 'POST':
+	        """User sent an email request"""
+	        msg = Message("Hello",
+                  sender="keighb@live.com",
+                  recipients=["scholarscarpak@gmail.com"])
+	        msg.body = "You have received new feedback from {0} {1} <{2}>.\n\n {3}".format(
+	            request.form['first-name'],
+	            request.form['last-name'],
+	            request.form['email'],
+	            request.form['message'])
+	        try:
+	            mail.send(msg)
+	            msg = "We will respond as soon as possible."
+	            category = "success"
+	        except Exception as err:
+	            msg = str(err)
+	            category = "danger"
+
+	        resp = {'feedback': msg, 'category': category}
+	        return make_response(jsonify(resp), 200)
+
+	    elif request.method == 'GET':
+	        """User is viewing the page"""
+	        return render_template('contact.html')
 
 
-	from .auth import auth_blueprint
-	app.register_blueprint(auth_blueprint)
+
 
 	return app
